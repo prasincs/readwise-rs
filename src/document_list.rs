f6aed480df54be00cc16255f260faa493c248eab
Prasanna Gautam <prasannagautam@gmail.com>
2023-07-16 06:15:50 -0700

    get all entries

--- src/document_list.rs
+++ src/document_list.rs
@@ -1,12 +1,13 @@
-use chrono::{Date, DateTime, NaiveDate, Utc};
+use chrono::{DateTime, NaiveDate, Utc};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
+use std::collections::HashMap;
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Root {
     pub count: i64,
-    pub next_page_cursor: String,
+    pub next_page_cursor: Option<String>,
     pub results: Vec<Result>,
 }
 
@@ -19,30 +20,27 @@ pub struct Result {
     pub source_url: String,
     pub title: String,
     pub author: String,
-    pub source: String,
+    pub source: Option<String>,
     pub category: String,
     pub location: String,
-    pub tags: Tags,
+    pub tags: HashMap<String, serde_json::Value>,
     #[serde(rename = "site_name")]
-    pub site_name: String,
+    pub site_name: Option<String>,
     #[serde(rename = "word_count")]
-    pub word_count: i64,
+    pub word_count: Option<i64>,
     #[serde(rename = "created_at")]
     pub created_at: DateTime<Utc>,
     #[serde(rename = "updated_at")]
     pub updated_at: DateTime<Utc>,
     pub notes: String,
     #[serde(rename = "published_date")]
-    pub published_date: NaiveDate,
-    pub summary: String,
+    // published_date can sometimes be unix time so cannot use NaiveDate
+    pub published_date: Value,
+    pub summary: Option<String>,
     #[serde(rename = "image_url")]
     pub image_url: Option<String>,
     #[serde(rename = "parent_id")]
     pub parent_id: Value,
     #[serde(rename = "reading_progress")]
-    pub reading_progress: Option<f64>,
+    pub reading_progress: f64,
 }
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Tags {}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate reqwest;
 use std::env;
+use std::{thread, time};
 
 use reqwest::header;
 
@@ -7,27 +8,70 @@ mod document_list;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let readwise_api_key = env::var("READWISE_API_KEY").ok().unwrap();
     let mut headers = header::HeaderMap::new();
-    headers.insert(
-        "Authorization",
-        format!("Token {}", readwise_api_key).parse().unwrap(),
-    );
-    headers.insert("Content-Type", "application/json".parse().unwrap());
-    let client = reqwest::blocking::Client::builder()
-        .redirect(reqwest::redirect::Policy::none())
-        .build()
-        .unwrap();
-    let res = client
-        .get("https://readwise.io/api/v3/list/?location=feed")
-        .headers(headers.clone())
-        .send()?
-        .text()?;
-    println!("{}", res);
-    let result: document_list::Root = client
-        .get("https://readwise.io/api/v3/list/?location=feed")
-        .headers(headers.clone())
-        .send()?
-        .json()?;
-    println!("{:#?}", result);
+    let mut cursor = "".to_string();
+    let mut done = false;
+    let mut docs = Vec::<document_list::Result>::new();
+    while !done {
+        headers.insert(
+            "Authorization",
+            format!("Token {}", readwise_api_key).parse().unwrap(),
+        );
+        headers.insert("Content-Type", "application/json".parse().unwrap());
+        let client = reqwest::blocking::Client::builder()
+            .redirect(reqwest::redirect::Policy::none())
+            .connection_verbose(true)
+            .build()
+            .unwrap();
+        let query = if cursor.is_empty() {
+            vec![("location", "feed")]
+        } else {
+            vec![("location", "feed"), ("pageCursor", cursor.as_str())]
+        };
+        let builder = client
+            .get("https://readwise.io/api/v3/list/")
+            .query(query.as_slice());
+
+        let builder2 = client
+            .get("https://readwise.io/api/v3/list/")
+            .query(query.as_slice());
+        let txt = builder2.headers(headers.clone()).send()?.text()?;
+        println!("{}", txt);
+        let result: document_list::Root = builder.headers(headers.clone()).send()?.json()?;
+        docs.extend(result.results);
+        cursor = match result.next_page_cursor {
+            Some(c) => c,
+            None => "".to_string(),
+        };
+        println!("cursor: {},count: {}", cursor, docs.len());
+        if cursor.is_empty() {
+            done = true
+        }
+        // TODO: properly handle rate limits
+        thread::sleep(time::Duration::from_secs(10));
+        println!("=================================")
+    }
+    println!("Num docs: {}", docs.len());
+    // headers.insert(
+    //     "Authorization",
+    //     format!("Token {}", readwise_api_key).parse().unwrap(),
+    // );
+    // headers.insert("Content-Type", "application/json".parse().unwrap());
+    // let client = reqwest::blocking::Client::builder()
+    //     .redirect(reqwest::redirect::Policy::none())
+    //     .build()
+    //     .unwrap();
+    // let res = client
+    //     .get("https://readwise.io/api/v3/list/?location=feed")
+    //     .headers(headers.clone())
+    //     .send()?
+    //     .text()?;
+    // println!("{}", res);
+    // let result: document_list::Root = client
+    //     .get("https://readwise.io/api/v3/list/?location=feed")
+    //     .headers(headers.clone())
+    //     .send()?
+    //     .json()?;
+    // println!("{:#?}", result.next_page_cursor);
 
     Ok(())
 }
